13b3709be205df5e14a20634e6cc0160b877157e
Rúben André Barreiro <ruben.barreiro.92@gmail.com>
2020-02-06 21:46:03 +0000

    Some minor changes added
    Some minor changes added

--- projects/threejs/3d-object-modelling-spin-of-a-particle/3d-object-modelling-spin-of-a-particle.js
+++ projects/threejs/3d-object-modelling-spin-of-a-particle/3d-object-modelling-spin-of-a-particle.js
@@ -1447,7 +1447,8 @@ function find_intersections_spin_down() {
                     document.getElementById(span_data_title_elem_id).style.display = "inline";
 
                     console.log(intersects[0].object[particle_spin_down_property_keys[i]]);
-                    console.log(intersects[0]);
+                    console.log(intersects[0].object);
+                    console.log(particle_spin_down_property_keys[i]);
                     
                     
                     var some_object_data_info = (intersects[0].object[particle_spin_down_property_keys[i]])[0];
